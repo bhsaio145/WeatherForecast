4b83f4f8534c87a6fd0a6c0e56637da6509d22a0
bhsaio145 <60438250+bhsaio145@users.noreply.github.com>
2024-05-05 15:32:58 -0400

    Update index.js

--- index.js
+++ index.js
@@ -2,11 +2,28 @@ const tempButton = document.getElementsByClassName("temperature_button")[0];
 
 tempButton.addEventListener("click" , () =>{
     if(tempButton.classList.contains("temp_C")){
-        console.log("C")
+        celToFaren()
         tempButton.classList.toggle("temp_C")
     }
     else{
-        console.log("F")
+        farenToCel()
         tempButton.classList.toggle("temp_C")
     }
 })
+
+function celToFaren(){
+    let tempCel = document.getElementsByClassName("FC")
+    var temp;
+    for(let i = 0 ; i < tempCel.length ; i++){
+        temp = parseInt(tempCel[i].textContent);
+        tempCel[i].textContent = Math.round((temp*9/5)+32);
+    }
+}
+function farenToCel(){
+    let tempFaren = document.getElementsByClassName("FC")
+    var temp;
+    for(let i = 0 ; i < tempFaren.length ; i++){
+        temp = parseInt(tempFaren[i].textContent);
+        tempFaren[i].textContent = Math.round((temp-32)*5/9);
+    }
+}
